9042db88193a02fb0fab929341d75db8a3cbbd04
Smriti Rani <47896316+Shield011@users.noreply.github.com>
2022-09-28 01:25:13 +0530

    Updates

---
+++ precache-manifest.64778930c691540e8b0e79f65157dcb8.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6285c540141ebc807909878e1b49cde3",
+    "url": "/Luxotica-website-final/index.html"
+  },
+  {
+    "revision": "2286990416a6a4db6e1e",
+    "url": "/Luxotica-website-final/static/css/2.99c1cb0e.chunk.css"
+  },
+  {
+    "revision": "0b0abd4700b88d78540e",
+    "url": "/Luxotica-website-final/static/css/main.78bc264e.chunk.css"
+  },
+  {
+    "revision": "2286990416a6a4db6e1e",
+    "url": "/Luxotica-website-final/static/js/2.207e3be5.chunk.js"
+  },
+  {
+    "revision": "3a31f49aea923712a3f4bb8f84858309",
+    "url": "/Luxotica-website-final/static/js/2.207e3be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0abd4700b88d78540e",
+    "url": "/Luxotica-website-final/static/js/main.1d171f24.chunk.js"
+  },
+  {
+    "revision": "f7e72b87e4e0baf8a01e",
+    "url": "/Luxotica-website-final/static/js/runtime-main.9a18a420.js"
+  },
+  {
+    "revision": "edce24dc49990fb1764e8264b772d4f7",
+    "url": "/Luxotica-website-final/static/media/HuracanBlue.edce24dc.png"
+  },
+  {
+    "revision": "e4b7138f1bfa08caf0b49fe3682dc302",
+    "url": "/Luxotica-website-final/static/media/LuxoticaLogo.e4b7138f.png"
+  },
+  {
+    "revision": "e01e90f1a49e836baa44d22505e337a4",
+    "url": "/Luxotica-website-final/static/media/MiniCooperRed.e01e90f1.png"
+  },
+  {
+    "revision": "be5101e8a554345ef2943e0c3bbb12c8",
+    "url": "/Luxotica-website-final/static/media/Unsplash9.be5101e8.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Luxotica-website-final/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Luxotica-website-final/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Luxotica-website-final/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Luxotica-website-final/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Luxotica-website-final/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "be834c8b2181aea1eed7b2b1e259239d",
+    "url": "/Luxotica-website-final/static/media/carDelivery.be834c8b.jpg"
+  },
+  {
+    "revision": "154a87d97de5a55fe055d7a9afd1f9c1",
+    "url": "/Luxotica-website-final/static/media/carHandover.154a87d9.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Luxotica-website-final/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Luxotica-website-final/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Luxotica-website-final/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Luxotica-website-final/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Luxotica-website-final/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Luxotica-website-final/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Luxotica-website-final/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Luxotica-website-final/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Luxotica-website-final/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Luxotica-website-final/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Luxotica-website-final/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "3c3dde48fbada5f06f54132fb64c7da0",
+    "url": "/Luxotica-website-final/static/media/phoneTalking.3c3dde48.jpg"
+  },
+  {
+    "revision": "4d629fe9433fbe45e24868f8950c172d",
+    "url": "/Luxotica-website-final/static/media/slider1.4d629fe9.jpg"
+  },
+  {
+    "revision": "3a46b6ef01384169e457742d8cfe3365",
+    "url": "/Luxotica-website-final/static/media/slider2.3a46b6ef.jpg"
+  },
+  {
+    "revision": "3858decb25f0dd398c2c60173c9c57f7",
+    "url": "/Luxotica-website-final/static/media/slider3.3858decb.jpg"
+  },
+  {
+    "revision": "f504afc6b8e60c5c401b9f23d51e45d9",
+    "url": "/Luxotica-website-final/static/media/slider4.f504afc6.jpg"
+  },
+  {
+    "revision": "7522f5f9aa4f75809f470aa1940a2a9b",
+    "url": "/Luxotica-website-final/static/media/visitingShowroom.7522f5f9.jpg"
+  }
+]);
